c7674d45beb86721964279add49a5c829a0ec89a
Gregory Casamento <greg.casamento@gmail.com>
2023-08-31 15:32:55 -0400

    Initial Commit

---
+++ NSView_subclass_awakeFromNib_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSView_subclass_awakeFromNib_test
+//
+//  Created by Gregory John Casamento on 8/31/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSView_subclass_awakeFromNib_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSView_subclass_awakeFromNib_test
+//
+//  Created by Gregory John Casamento on 8/31/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
